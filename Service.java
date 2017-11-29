7f13477d73a735f43afd770d4be29eb017393db2
srdelisser <srdelisser@live.ca>
2017-11-29 16:44:48 -0500

    Update Service.java

--- Service.java
+++ Service.java
@@ -2,6 +2,8 @@ public class Service
 {
 	Schedule schedule;
 	String type;
+	
+	public Service( 
 
 	//implement constructor which should take in daily hours and then make a schedule from that class
 	//might do without the shop class lmk what you think
