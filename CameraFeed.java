2a2e6ced01edec184c9212a3798e397e58735d21
srdelisser <srdelisser@live.ca>
2017-11-29 15:01:54 -0500

    Update CameraFeed.java

--- CameraFeed.java
+++ CameraFeed.java
@@ -4,4 +4,22 @@ public class CameraFeed
 	boolean eventTriggered;
 
 	//constructor needs to take only the location
+	public CameraFeed(String location){
+		location=location;
+	}
+	
+	public String getLocation(){
+		return location;
+	}
+	public void setLocation(String loc){
+		location=loc;
+	}
+	
+	public boolean getEventTriggered(){
+		return eventTriggered;
+	}
+	public void setEventTriggered(boolean trig){
+		eventTriggered=trig;
+	}
+	
 }
