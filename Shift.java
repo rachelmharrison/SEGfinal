3bd4f3bc3556f7b3167205ce3938f949d2a0b185
srdelisser <srdelisser@live.ca>
2017-11-29 16:40:09 -0500

    Update Shift.java

--- Shift.java
+++ Shift.java
@@ -9,6 +9,22 @@ public class Shift
 	int day;
 
 	//constructor takes employee, startTime, endTime, month, day
-
+	public Shift(Personnel employee, String st, String et, String m, int d){
+		employee=employee;
+		startTime=st;
+		endTime=et;
+		month=m;
+		day=d;
+		timeIn=-1;
+		timeOut=-1;
+	}
 	//implement clock in/out methods that set timeIn and timeOut
+	public void clockIn(String time){
+		timeIn=time;
+	}
+	public void clockOut(String time){
+		if(timeIn!=-1){
+			timeOut=time;
+		}
+	}
 }
