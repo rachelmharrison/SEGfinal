a85ac69506d65856149a428bfb9d5364458ad257
srdelisser <srdelisser@live.ca>
2017-11-29 14:58:39 -0500

    Update Booking.java

--- Booking.java
+++ Booking.java
@@ -9,10 +9,37 @@ public class Booking
 	String seatClass;
 
 
-	Booking(FLight f, String s;=, String c)
+	Booking(Flight f, String s, String c)
 	{
 		//implement, seat should be -1 if still unassigned, assign luggage variables based on class
+		flight=f;
+		seat=-1;
+		seatClass=c;//what are the different claases 
+		if(seatClass=="First Class"){
+			carryOnAllowed=3;
+			checkBagsAllowed=3;
+			carryOnWeight=50;
+			checkedBagWeight=20;
+		}
+		if(seatClass=="Business Class"){
+			carryOnAllowed=2;
+			checkBagsAllowed=2;
+			carryOnWeight=50;
+			checkedBagWeight=20;
+		}
+		if(seatClass=="Economy Class"){
+			carryOnAllowed=1;
+			checkBagsAllowed=2;
+			carryOneWeight=50;
+			checkedBagWeight=20;
+		}
 	}
 
 	//implement getter and the setter for seat
+	public String getSeat(){
+		return seat;
+	}
+	public void setSeat(String seat){
+		seat=seat;
+	}
 }
