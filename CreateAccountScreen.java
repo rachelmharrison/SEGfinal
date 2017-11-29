2ebf306a0d59be24f5709ddcabdb310c946b5d53
rachelmharrison <rachel.m.harrison8@gmail.com>
2017-11-29 16:09:40 -0500

    LogOnScreen created

--- CreateAccountScreen.java
+++ CreateAccountScreen.java
@@ -4,8 +4,40 @@ import java.awt.event.*;
 
 public class CreateAccountScreen extends JPanel
 {
+	JTextField usernameField;
+	JPasswordField passwordField1;
+	JPasswordField passwordField2;
+	JTextField nameField;
+	JComboBox roleBox;
+	JComboBox genderBox;
+	JTextField dobField;
+
+
 	CreateAccountScreen(View view)
 	{
+		this.view=view;
+		JLabel usernameLabel=new JLabel("Username: ");
+		JLabel passwordLabel1=new JLabel("Password: ");
+		JLabel passwordLabel2=new JLabel("Confirm Password: ");
+		JLabel nameLabel=new JLabel("Name: ");
+		JLabel roleLabel=new JLabel("Type of Account: ");
+		JLabel genderLabel=new JLabel("Gender (if passenger): ");
+		JLabel dobLabel=new Jlabel("Date of Birth YYYY/MM/DD (if passenger): ");
 
+		usernameField=new JTextField()
+
+
+		usernameField.setColumns(10);
+		passwordField1.setColumns(10);
+		passwordField2.setColumns(10);
+		nameField.setColumns(10);
+		dobField.setColumns(10);
+	
+
+
+		usernameLabel.setFont(new Font("Arial", Font.PLAIN, 30));
+		passwordLabel.setFont(new Font("Arial", Font.PLAIN, 30));
+		usernameField.setFont(new Font("Arial", Font.PLAIN, 30));
+		passwordField.setFont(new Font("Arial", Font.PLAIN, 30));
 	}
 }
--- UserAccount.java
+++ UserAccount.java
@@ -2,31 +2,35 @@ public class UserAccount
 {
 	String userName;
 	String password;
+	String email;
 	Passenger passengerRole;
 	Personnel employeeRole;
 	Admin adminRole;
 
-	UserAccount(String userName, String password, Passenger p);
+	UserAccount(String userName, String password, Passenger p, String email);
 	{
 		this.userName=userName;
 		this.password=password;
 		this.passengerRole=p;
+		this.email=email;
 		system.addPassenger(passengerRole);
 	}
 
-	UserAccount(String userName, String password, Personnel p);
+	UserAccount(String userName, String password, Personnel p, String email);
 	{
 		this.userName=userName;
 		this.password=password;
 		this.employeeRole=p;
+		this.email;
 		system.addEmployee(employeeRole);
 	}
 
-	UserAccount(String userName, String password, Admin a);
+	UserAccount(String userName, String password, Admin a, String email);
 	{
 		this.userName=userName;
 		this.password=password;
 		this.adminRole;
+		this.email=email;
 		system.addAdmin(adminRole);
 	}
 
