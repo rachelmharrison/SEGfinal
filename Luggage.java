f7a4b2c53f08f3eb1b8844391c48191cc4277c25
srdelisser <srdelisser@live.ca>
2017-11-29 13:08:05 -0500

    Update Luggage.java

--- Luggage.java
+++ Luggage.java
@@ -10,7 +10,28 @@ public class Luggage
 	{
 		//implement
 		//isLost should be set to false initially, location should be "checked in"
+		isLost=false;
+		passenger=passenger;
+		flight=flight;
+		weight=weight;
+		location="checked in";
+		
+		
 	}
 
 	//implement getters and a setter for isLost and location
+	
+	public boolean getIsLost(){
+		return isLost;
+	}
+	public void setIsLost(boolean lost){
+		isLost=lost;
+	}
+	
+	public String getLocation(){//send me your location-Khalid
+		return location;
+	}
+	public void setLocation(String loc){
+		location=loc;
+	}
 }
