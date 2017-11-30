86d498bf69abaf3b9572145d78fedc26e8664a6e
srdelisser <srdelisser@live.ca>
2017-11-30 15:42:42 -0500

    Update Personnel.java

--- Personnel.java
+++ Personnel.java
@@ -3,21 +3,26 @@ public class Personnel
 	String role;
 	Shift[] shifts=new Shift[100];
 	Shift currentShift;
-
+	int shiftCounter=0;
 
 	Personnel(String role)
 	{
 		this.role=role;
 	}
 
-	public void addShift()
-	{
-		//implement
+	public void addShift(Shift s)
+	{	//implement
+		if (shiftCounter!=100){
+			Shift[shitfCounter]=s;
+			shiftCounter++;
+		}
 	}
 
 	public void clockIn(Shift shift)
 	{
 		//should set current shift and clock-in time in that object
+		String time=new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+		shift.clockIn(time);
 	}
 
 	public void clockOut()
@@ -25,5 +30,8 @@ public class Personnel
 		//should only be possible if clocked in
 		//store time in currentShift object
 		//reset currentShift 
+		String time= new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+		shift.clockOut(time);
+
 	}
 }
