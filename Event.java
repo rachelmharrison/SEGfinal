fd295fdbe5809a9623eb76dafdde4ce93c5c2b4d
srdelisser <srdelisser@live.ca>
2017-11-30 14:43:43 -0500

    Update Event.java

--- Event.java
+++ Event.java
@@ -1,4 +1,30 @@
 public class Event
 {
 	//this one is really easy just make a constructor that takes in all the requirments from and event log and store them
+	String date;
+	String time;
+	String category;
+	String location;
+	String description;
+	Personnel[] guardsPresent=new Personnel[25];
+	Personnel guardInCharge;
+	
+	
+	public Event(String d,String t,String c, String l, String des, Personnel g){
+		date=d;
+		time=t;
+		category=c;
+		location=l;
+		description=des;
+		guardInCharge=g;
+	}
+	
+	public void addGuard(Personnel g){
+		
+	}
+	public void setDate(String d){
+		date=d;
+	}
+	
+	
 }
