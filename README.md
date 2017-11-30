e280fb5a2cf0d407e5d33112789ffa8a5ed61d18
rachelmharrison <rachel.m.harrison8@gmail.com>
2017-11-30 14:30:23 -0500

    readme things

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # SEG
 View - add code that validates format of date of birth (in actionPerformed)
+
 MainClass - implement create user method, this ivolved creating a new user and adding to the accounts array and
             and then creating eiter a new passenger, admin, or employee and adding to the respective array
+
 View - compile and fix errors (mainly in flight class) ignore errors that say ViewSOmethingsScreen don't exist
+
+Event - I really need this construtor done
--- ViewPersonnelScreen.java
+++ ViewPersonnelScreen.java
@@ -1,18 +1,20 @@
 public class ViewPersonnelScreen extends SearchScreen
 {
-	String category="Passenger Name:";
+	String category="Employee Name:";
 	int n=3;
-	String[][] m=new String[3][2];
+	String[][] m=new String[3][6];
 	m[0][0]="Yes";
 	m[0][1]="No";
-	m[1][0]="Yes";
-	m[1][1]="No";
-	m[2][0]="Yes";
-	m[2][1]="No";
+	m[1][0]="Airline Staff";
+	m[1][1]="Luggage";
+	m[1][2]="Cleaning";
+	m[1][3]="Catering";
+	m[1][4]="Shops";
+	m[1][5]="Security";
 
-	String[] s={"At Airport", "In Air", "Is High Risk"};
+	String[] s={"At Work", "Service"};
 
-	ViewPassengersScreen(View view)
+	ViewPersonnelScreen(View view)
 	{
 		super(category, n, m, s, view);
 	}
